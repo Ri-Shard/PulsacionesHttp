565a878456a89ed7320ec57d15359f30b7ee8bd3
“Ri-Shard <rikarudok@gmail.com>
2020-03-27 21:22:40 -0500

    first commit

---
+++ ClientApp/src/app/pulsacion/persona-registro/persona-registro.component.ts
@@ -0,0 +1,27 @@
+import { PersonaService } from '../../Services/persona.service';
+import { Persona } from '../models/persona';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persona-registro',
+  templateUrl: './persona-registro.component.html',
+  styleUrls: ['./persona-registro.component.css']
+})
+export class PersonaRegistroComponent implements OnInit {
+
+  persona : Persona;
+  constructor(private personaService: PersonaService) { }
+
+  ngOnInit(){
+    this.persona = new Persona;
+  }
+
+  add(){
+    this.personaService.post(this.persona).subscribe(p => {
+      if(p != null){
+        alert('Persona creada!');
+        this.persona = p;
+      }
+    });
+  }
+}
